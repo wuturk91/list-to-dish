fa39b96bbdb25b3a5e07e2de41d1c17daf3cc47b
wuturk91 <cemsalih91@gmail.com>
2026-02-12 02:11:19 +0000

    remove unused font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/Atoms/Header";
+import Header from "./components/Atoms/Header/Header";
 import Providers from "./providers";
 
 const geistMono = Geist_Mono({
