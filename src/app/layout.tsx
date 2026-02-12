cb3d02d4b0b17264891affde7b51a95289e47b03
wuturk91 <cemsalih91@gmail.com>
2026-02-12 02:22:10 +0000

    Reorganize component structure; moved shared code to src root

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Atoms/Header/Header";
-import Providers from "./providers";
+import Providers from "./components/providers";
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
